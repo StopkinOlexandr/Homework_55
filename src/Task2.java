e7bb388d7e79aa03d93277c7af1b296dd8e423c2
AlexandrStopkin <stnalex1@gmail.com>
2023-04-05 14:39:03 +0200

    Task 2. Shortest words

--- src/Task2.java
+++ src/Task2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 // Задача 2* (не обязательно).
@@ -11,21 +12,28 @@ import java.util.List;
 // Оценить временную и пространственную сложность алгоритма.
 public class Task2 {
   public static void main(String[] args) throws IOException { // O(n) - time, O(n) - space
+    List<String> list = new ArrayList<>();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringBuilder result = new StringBuilder();
     int shortestLength = -1;
     System.out.print("Введите количество слов: ");
     int n = Integer.parseInt(br.readLine());
     System.out.println("Введите " + n + " слов, каждое с новой строки:");
-    for (int i = 0; i < n; ++i) { // O(n) - time, O(1) - space
+    for (int i = 0; i < n; ++i) { // O(n) - time, O(n) - space
       String input = br.readLine();
+      list.add(input);
       if (shortestLength == -1) {
-        result.append(input);
         shortestLength = input.length();
-      } else if (input.length() <= shortestLength) {
-        result.insert(0, input + " ");
-      } else {
-        result.append(" " + input);
+      } else if (input.length() < shortestLength) {
+        shortestLength = input.length();
+      }
+    }
+    for (String word: list) { // O(n) - time, O(1) - space
+      if(word.length() == shortestLength) {
+        if (!result.isEmpty()) {
+          result.append(" ");
+        }
+        result.append(word);
       }
     }
     System.out.println("Результирующая строка:");
